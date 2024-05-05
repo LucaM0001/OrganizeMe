b40aec9e0db106ba5fe20df22d392ad81b34be3e
Luca Mamitiana RANDRIANIAINA <lucamamitiana@gmail.com>
2024-05-05 21:54:47 +0300

    Recuperation des taches terminee

--- src/components/Tasks/Task/Task.jsx
+++ src/components/Tasks/Task/Task.jsx
@@ -1,21 +1,21 @@
 import { PenFill, TrashFill } from "react-bootstrap-icons";
 
-const Task = (props) => {
+const Task = ({ id, name, isCompleted }) => {
   return (
-    <li className="list-group-item">
+    <>
       <div>
         <input type="checkbox" className="form-check-input" />
-        <span className="ms-3">Task Name</span>
+        <span className="ms-3">{name}</span>
       </div>
       <div>
-        <button className="btn btn-danger me-2">
+        <button className="btn btn-danger me-2" onClick={() => console.log(id)}>
           <TrashFill />
         </button>
-        <button className="btn btn-warning">
+        <button className="btn btn-warning" onClick={() => console.log(id)}>
           <PenFill color="white" />
         </button>
       </div>
-    </li>
+    </>
   );
 };
 
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,8 +1,32 @@
 import { List, ListCheck, ListTask } from "react-bootstrap-icons";
 import AddForm from "./AddForm/AddForm";
 import Task from "./Task/Task";
+import { useState } from "react";
 
 const Tasks = (props) => {
+  const [tasks, setTasks] = useState([
+    {
+      id: Date.now(),
+      name: "Task1",
+      isCompleted: false,
+    },
+    {
+      id: Date.now() * 2,
+      name: "Task2",
+      isCompleted: false,
+    },
+  ]);
+
+  const handleShowTask = () => {
+    let tasksArray = [...tasks];
+
+    return tasksArray.map((task) => (
+      <li key={task.id} className="list-group-item">
+        <Task {...task} />
+      </li>
+    ));
+  };
+
   return (
     <div id="tasks">
       <h1 className="text-center text-primary" id="title">
@@ -20,11 +44,7 @@ const Tasks = (props) => {
           <ListTask size={22} />
         </button>
       </div>
-      <ul className="list-group">
-        <Task />
-        <Task />
-        <Task />
-      </ul>
+      <ul className="list-group">{handleShowTask()}</ul>
     </div>
   );
 };
