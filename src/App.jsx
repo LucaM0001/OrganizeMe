edc95e77c48f5debefa8cb7ac73c69b899d4b3f0
Luca Mamitiana RANDRIANIAINA <lucamamitiana@gmail.com>
2024-05-05 21:09:01 +0300

    Configuration du projet

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+
+const App = () => {
+  return <div className="container">App</div>;
+};
+
+export default App;
